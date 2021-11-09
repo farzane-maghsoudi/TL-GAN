c85c50a17d925ade7c4ddaa63dd2a0adb82671cd
farzane-maghsoudi <49396097+farzane-maghsoudi@users.noreply.github.com>
2021-11-09 23:26:21 +0330

    Update networks.py

--- networks.py
+++ networks.py
@@ -4,137 +4,6 @@ from torch.nn.parameter import Parameter
 from torchvision import models
 from block import fusions
 
-class ResnetGenerator(nn.Module):
-    def __init__(self, input_nc, output_nc, ngf=64, n_blocks=6, img_size=256, light=False):
-        assert(n_blocks >= 0)
-        super(ResnetGenerator, self).__init__()
-        self.input_nc = input_nc
-        self.output_nc = output_nc
-        self.ngf = ngf
-        self.n_blocks = n_blocks
-        self.img_size = img_size
-        self.light = light
-
-        n_downsampling = 2
-
-        mult = 2**n_downsampling
-        UpBlock0 = [nn.ReflectionPad2d(1),
-                nn.Conv2d(int(ngf * mult / 2), ngf * mult, kernel_size=3, stride=1, padding=0, bias=True),
-                ILN(ngf * mult),
-                nn.ReLU(True)]
-
-        self.relu = nn.ReLU(True)
-
-        # Gamma, Beta block
-        if self.light:
-            FC = [nn.Linear(ngf * mult, ngf * mult, bias=False),
-                  nn.ReLU(True),
-                  nn.Linear(ngf * mult, ngf * mult, bias=False),
-                  nn.ReLU(True)]
-        else:
-            FC = [nn.Linear(img_size // mult * img_size // mult * ngf * mult, ngf * mult, bias=False),
-                  nn.ReLU(True),
-                  nn.Linear(ngf * mult, ngf * mult, bias=False),
-                  nn.ReLU(True)]
-        self.gamma = nn.Linear(ngf * mult, ngf * mult, bias=False)
-        self.beta = nn.Linear(ngf * mult, ngf * mult, bias=False)
-
-        # Up-Sampling Bottleneck
-        for i in range(n_blocks):
-            setattr(self, 'UpBlock1_' + str(i+1), ResnetAdaILNBlock(ngf * mult, use_bias=False))
-
-        # Up-Sampling
-        UpBlock2 = []
-        for i in range(n_downsampling):
-            mult = 2**(n_downsampling - i)
-            # Experiments show that the performance of Up-sample and Sub-pixel is similar,
-            #  although theoretically Sub-pixel has more parameters and less FLOPs.
-            # UpBlock2 += [nn.Upsample(scale_factor=2, mode='nearest'),
-            #              nn.ReflectionPad2d(1),
-            #              nn.Conv2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=1, padding=0, bias=False),
-            #              ILN(int(ngf * mult / 2)),
-            #              nn.ReLU(True)]
-            UpBlock2 += [nn.ReflectionPad2d(1),   
-                         nn.Conv2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=1, padding=0, bias=False),
-                         ILN(int(ngf * mult / 2)),
-                         nn.ReLU(True),
-                         nn.Conv2d(int(ngf * mult / 2), int(ngf * mult / 2)*4, kernel_size=1, stride=1, bias=True),
-                         nn.PixelShuffle(2),
-                         ILN(int(ngf * mult / 2)),
-                         nn.ReLU(True)
-                         ]
-
-        UpBlock2 += [nn.ReflectionPad2d(3),
-                     nn.Conv2d(ngf, output_nc, kernel_size=7, stride=1, padding=0, bias=False),
-                     nn.Tanh()]
-
-        self.FC = nn.Sequential(*FC)
-        self.UpBlock0 = nn.Sequential(*UpBlock0)
-        self.UpBlock2 = nn.Sequential(*UpBlock2)
-
-    def forward(self, z):
-        x = z
-        x = self.UpBlock0(x)
-
-        if self.light:
-            x_ = torch.nn.functional.adaptive_avg_pool2d(x, 1)
-            x_ = self.FC(x_.view(x_.shape[0], -1))
-        else:
-            x_ = self.FC(x.view(x.shape[0], -1))
-        gamma, beta = self.gamma(x_), self.beta(x_)
-
-        for i in range(self.n_blocks):
-            x = getattr(self, 'UpBlock1_' + str(i+1))(x, gamma, beta)
-
-        out = self.UpBlock2(x)
-
-        return out
-
-
-class ResnetBlock(nn.Module):
-    def __init__(self, dim, use_bias):
-        super(ResnetBlock, self).__init__()
-        conv_block = []
-        conv_block += [nn.ReflectionPad2d(1),
-                       nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=0, bias=use_bias),
-                       nn.InstanceNorm2d(dim),
-                       nn.ReLU(True)]
-
-        conv_block += [nn.ReflectionPad2d(1),
-                       nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=0, bias=use_bias),
-                       nn.InstanceNorm2d(dim)]
-
-        self.conv_block = nn.Sequential(*conv_block)
-
-    def forward(self, x):
-        out = x + self.conv_block(x)
-        return out
-
-
-class ResnetAdaILNBlock(nn.Module):
-    def __init__(self, dim, use_bias):
-        super(ResnetAdaILNBlock, self).__init__()
-        self.pad1 = nn.ReflectionPad2d(1)
-        self.conv1 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=0, bias=use_bias)
-        self.norm1 = adaILN(dim)
-        self.relu1 = nn.ReLU(True)
-
-        self.pad2 = nn.ReflectionPad2d(1)
-        self.conv2 = nn.Conv2d(dim, dim, kernel_size=3, stride=1, padding=0, bias=use_bias)
-        self.norm2 = adaILN(dim)
-
-    def forward(self, x, gamma, beta):
-        out = self.pad1(x)
-        out = self.conv1(out)
-        out = self.norm1(out, gamma, beta)
-        out = self.relu1(out)
-        out = self.pad2(out)
-        out = self.conv2(out)
-        out = self.norm2(out, gamma, beta)
-
-        return out + x
-
-
 class adaILN(nn.Module):
     def __init__(self, num_features, eps=1e-5, momentum=0.9, using_moving_average=True, using_bn=False):
         super(adaILN, self).__init__()
@@ -470,24 +339,21 @@ class Generator(nn.Module):
         self.relu = nn.ReLU(True)
 
         # Gamma, Beta block
-        if self.light:
-            FC = [nn.Linear(ngf * mult, ngf * mult, bias=False),
-                  nn.ReLU(True),
-                  nn.Linear(ngf * mult, ngf * mult, bias=False),
-                  nn.ReLU(True)]
-        else:
-            FC = [nn.Linear(img_size // mult * img_size // mult * ngf * mult, ngf * mult, bias=False),
-                  nn.ReLU(True),
-                  nn.Linear(ngf * mult, ngf * mult, bias=False),
-                  nn.ReLU(True)]
-        self.gamma = nn.Linear(ngf * mult, ngf * mult, bias=False)
-        self.beta = nn.Linear(ngf * mult, ngf * mult, bias=False)
-		
-		self.gamma = nn.Linear(ngf * mult, ngf * mult, bias=False)
-        self.beta = nn.Linear(ngf * mult, ngf * mult, bias=False)
+        #if self.light:
+        #    FC = [nn.Linear(ngf * mult, ngf * mult, bias=False),
+        #          nn.ReLU(True),
+        #          nn.Linear(ngf * mult, ngf * mult, bias=False),
+        #          nn.ReLU(True)]
+        #else:
+        #    FC = [nn.Linear(img_size // mult * img_size // mult * ngf * mult, ngf * mult, bias=False),
+        #          nn.ReLU(True),
+        #          nn.Linear(ngf * mult, ngf * mult, bias=False),
+        #          nn.ReLU(True)]
+        #self.gamma = nn.Linear(ngf * mult, ngf * mult, bias=False)
+        #self.beta = nn.Linear(ngf * mult, ngf * mult, bias=False)
 
         # attention Bottleneck
-		attention = AttentionBlock(ngf * mult, use_bias=False)
+	#	attention = AttentionBlock(ngf * mult, use_bias=False)
 		attention = MultiSelfAttentionBlock(ngf, ngf * mult)
 		
 		UpBlock1 = [nn.ReflectionPad2d(1),
@@ -534,15 +400,17 @@ class Generator(nn.Module):
         x = self.UpBlock0(x)
 		y = x
 
-        if self.light:
-            x_ = torch.nn.functional.adaptive_avg_pool2d(x, 1)
-            x_ = self.FC(x_.view(x_.shape[0], -1))
-        else:
-            x_ = self.FC(x.view(x.shape[0], -1))
-        gamma, beta = self.gamma(x_), self.beta(x_)
+        #if self.light:
+        #    x_ = torch.nn.functional.adaptive_avg_pool2d(x, 1)
+        #    x_ = self.FC(x_.view(x_.shape[0], -1))
+        #else:
+        #    x_ = self.FC(x.view(x.shape[0], -1))
+        #gamma, beta = self.gamma(x_), self.beta(x_)
 
         for i in range(self.n_blocks):
-            x = attention(x, ngf * mult, gamma, beta, use_bias=False)
+		x = attention(x)
+        #    x = attention(x, ngf * mult, gamma, beta, use_bias=False)
+		
 		x = self.UpBlock1(x)
 		x = x + y
 		
