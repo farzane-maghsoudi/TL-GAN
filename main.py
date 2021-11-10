77647859ef503ded5204baf3e4c6aaaf8412ea3e
farzane-maghsoudi <49396097+farzane-maghsoudi@users.noreply.github.com>
2021-11-10 13:04:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def main():
     gan.build_model()
     
     # build pretraind resnet
-		resnet_pre = NewResnet(output_layers = [0,1,2,3,4,5,6,7,8,9])
+    resnet_pre = NewResnet(output_layers = [0,1,2,3,4,5,6,7,8,9])
 
     if args.phase == 'train' :
         gan.train()
